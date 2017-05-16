3f8c3804b4110b543cef86ffbc5a19594b515457
Mateusz Kwansy <mkwasny97@gmail.com>
2017-05-17 01:19:14 +0200

    Podstawy aplikacji

---
+++ App2/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+
+namespace CryptMe
+{
+    [Activity(Label = "CryptMe", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            TextView inputTextView = FindViewById<EditText>(Resource.Id.InputTextView);
+            Button button_q = FindViewById<Button>(Resource.Id.buttonQ);
+
+            /*button_q.Click += (object sender, EventArgs e) =>
+            {
+                inputTextView.Text += "Q";
+            };*/
+            
+        }
+    }
+}
+
---
+++ ContactsHandle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Foundation;
+using Contacts;
+using ContactsUI;
+using UIKit;
+
+namespace ContactAccess
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+
+        partial void FindContact_TouchUpInside(UIButton sender, string FamilyName)
+        {
+            // Create predicate to locate requested contact
+            var predicate = CNContact.GetPredicateForContacts(FamilyName);
+
+            // Define fields to be searched
+            var fetchKeys = new NSString[] { CNContactKey.GivenName, CNContactKey.FamilyName };
+
+            // Grab matching contacts
+            var store = new CNContactStore();
+            NSError error;
+            var contacts = store.GetUnifiedContacts(predicate, fetchKeys, out error);
+
+            // Found?
+            if (contacts.Length > 0)
+            {
+                // Get the first matching contact
+                var contact = contacts[0];
+
+                // Display it
+                FoundContact.Text = string.Format("{0} {1}", contact.GivenName, contact.FamilyName);
+            }
+            else
+            {
+                // Not found
+                FoundContact.Text = "";
+            }
+        }
+    }
+}
