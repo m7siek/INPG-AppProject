21e55a498d0e35dc30f5e5c8c58a7cd6565ec85a
Skngg <mkwasny97@gmail.com>
2017-05-18 13:25:59 +0200

    Modyfikacja interfejsu klawiatury i dodanie mechanizmu obsługującego

---
+++ CryptMe/KeyPress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+namespace CryptMe
+{
+    public static class KeyPress
+    {
+        public static string ModifyString(Button sender, string text)
+        {
+
+            if (sender.Id == Resource.Id.buttonSpace) return text+" ";
+            else if (sender.Id == Resource.Id.buttonBSP) return text.Remove(text.Length - 1);
+            else return text+sender.Text;
+        }
+    }
+}
--- CryptMe/MainActivity.cs
+++ CryptMe/MainActivity.cs
@@ -3,6 +3,8 @@ using Android.App;
 using Android.Content;
 using Android.Widget;
 using Android.OS;
+using Android.Content.Res;
+
 
 namespace CryptMe
 {
@@ -14,10 +16,46 @@ namespace CryptMe
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
 
-            
+            TextView inputTextView = FindViewById<TextView>(Resource.Id.InputTextView);
+
+            string inputtext = inputTextView.Text;
+
+            var buttons = new Button[40];
+
+            // Declare handle and behaviour for axml buttons
+            for (int i = 0; i < 40; i++)
+            {
+                string buttonID = "button";
+                if (i >= 10 && i < 36)
+                {
+                    char ID = (char)(65 + i - 10);
+                    buttonID += ID;
+                }
+                else if (i == 36) buttonID += "Space";
+                else if (i == 37) buttonID += "BSP";
+                else if (i == 38) buttonID += "Switch";
+                else if (i == 39) buttonID += "Enter";
+                else buttonID += i;
+
+                int resID = Resources.GetIdentifier(buttonID, "id", "CryptMe.CryptMe");
+                buttons[i] = (Button)FindViewById<Button>(resID);
+
+                Button activebutton = buttons[i];
+                
+
+                if (i >= 0 && i <= 37)
+                {
+                    
+                    buttons[i].Click += (object sender, EventArgs e) =>
+                    {
+                        inputTextView.Text = KeyPress.ModifyString(activebutton,inputTextView.Text);
+                    };
+                }
+            }
         }
     }
 }
+
 
