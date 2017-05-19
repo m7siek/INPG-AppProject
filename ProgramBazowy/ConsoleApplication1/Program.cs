74d1c36ca3e5e914f528083eef4201e4b8827785
milwa97 <marialewandowska1997@gmail.com>
2017-05-19 11:53:08 +0200

    Program bazowy pod generację klucza (zmienne są z wpisane w program)

---
+++ ProgramBazowy/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k;
+            long p, q, n, d, e ;
+
+            k = 3;
+
+            p = 179424673;
+
+            q = 179424691;
+
+            n = p*q;
+
+            e = 15485863;
+
+            d = (1 + k*(p-1)*(q-1) ) / e;
+
+
+
+
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+
+            Console.WriteLine("RSA: Key generator\n\np:"+p+"\nq:"+q+"\nn:"+n+"\ne:"+e+"\nd:"+d);
+
+
+            Console.ReadKey(); 
+
+
+        }
+    }
+}
