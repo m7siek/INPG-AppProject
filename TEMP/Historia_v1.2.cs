f3c798656437b2cca715c9bfca949edb63966c99
Jacobo <zaczkowskiinpg@gmail.com>
2017-05-19 18:59:06 +0200

    Formatowanie kodu Zmiany ułatwaijace czytelnosc Usunąłem wszystkie linie odpowiadające za działanie programu, zostawiacjac tylko kod funkcji Wprowadziłem niewielkie poprawki

--- TEMP/Historia_v1.2.cs
+++ TEMP/Historia_v1.2.cs
@@ -1,58 +1,40 @@
-﻿//Druga wersja funkcji
-//Odczyt z pliku i dodanie jednej linii z drugiego pliku
+﻿//Wersja do weryfikacji
 
+//Funkcja otwiera dwa pliki, historię i plik wyjsciowy
+//laczy je w jeden, wpisujac tresc pliku wyjsciowego na poczatek histori
+//Przechowuje do 100 pozcji
 
-//Funkcja odczytuje linie z pliku i przesuwa je o jedna linie w dol
-//w miejsce pierwszej linii wstawiajac pierwsza linie z drugiego plik
-//s1 - plik archiwum
-//s2 - plik wyjsciowy
-
-//Zmiana: Historia może przechowywać do 100 pozycji
-
-
+//Do ewentuanej zmiany: 
+//                      -nazwa pliku wyjsciowego dodana jako arument wywolania
+//                      -zmiana pojemnosci historii na nielimitowana
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
-namespace Historia_Konsolowo
+static void archive()
 {
-    class Program
-    {
-        static void Main()
+    String[] tmp_string = new String[100];
+    int i;
+
+        // Otwarcie strumieni
+        StreamReader sr1 = new StreamReader("archive.txt");
+        StreamReader sr2 = new StreamReader("application_output.txt");
+
+        //Odczyt po jednej linijce
+        for (i = 1; i < 100; i++)
         {
-            String[] tmp_string;
-
-            tmp_string = new String[100];
-            int i;
-
-            // Otwarcie strumieni
-            StreamReader sr1 = new StreamReader("s1.txt");
-            StreamReader sr2 = new StreamReader("s2.txt");
-
-            //Odczyt po jednej linijce
-            for (i = 1; i < 100; i++)
-            {
-                tmp_string[i] = sr1.ReadLine();
-
-            }
-            tmp_string[0] = sr2.ReadLine();
-
-            //Zapis do pliku
-            for (i = 0; i < 100; i++) Console.WriteLine(tmp_string[i]);
-
-            //Zamknięcie obu plików
-            sr1.Close();
-            sr2.Close();
-
-            //Zapis do pliku archiwalnego
-            File.WriteAllLines("s1.txt", tmp_string);
-
-            Console.ReadLine();
-
+            tmp_string[i] = sr1.ReadLine();
         }
-    }
+
+        tmp_string[0] = sr2.ReadLine();
+
+        //Zapis do pliku
+        for (i = 0; i < 100; i++) Console.WriteLine(tmp_string[i]);
+
+        //Zamknięcie obu plików
+        sr1.Close();
+        sr2.Close();
+
+        //Zapis do pliku archiwalnego
+        File.WriteAllLines("archive.txt", tmp_string);
 }
