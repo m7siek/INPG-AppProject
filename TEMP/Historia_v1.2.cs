9684d47a5fb8bc8c18611d75d85177ba60b57c8f
Jacobo <zaczkowskiinpg@gmail.com>
2017-05-19 18:47:02 +0200

    Rozszerzenie historii, może teraz przechowywać do 100 pozycji

--- TEMP/Historia_v1.2.cs
+++ TEMP/Historia_v1.2.cs
@@ -7,6 +7,8 @@
 //s1 - plik archiwum
 //s2 - plik wyjsciowy
 
+//Zmiana: Historia może przechowywać do 100 pozycji
+
 
 
 using System;
@@ -22,29 +24,34 @@ namespace Historia_Konsolowo
     {
         static void Main()
         {
-            String[] line, tmp_string;
-            line = new String[11];
-            tmp_string = new String[11];
+            String[] tmp_string;
+
+            tmp_string = new String[100];
             int i;
 
-            // Otwarcie pliku
+            // Otwarcie strumieni
             StreamReader sr1 = new StreamReader("s1.txt");
             StreamReader sr2 = new StreamReader("s2.txt");
 
             //Odczyt po jednej linijce
-            for (i = 1; i < 10; i++)
+            for (i = 1; i < 100; i++)
             {
                 tmp_string[i] = sr1.ReadLine();
+
             }
             tmp_string[0] = sr2.ReadLine();
 
-            for (i = 0; i < 10; i++) Console.WriteLine(tmp_string[i]);
-            sr1.Close();
+            //Zapis do pliku
+            for (i = 0; i < 100; i++) Console.WriteLine(tmp_string[i]);
 
+            //Zamknięcie obu plików
+            sr1.Close();
+            sr2.Close();
+
+            //Zapis do pliku archiwalnego
             File.WriteAllLines("s1.txt", tmp_string);
 
-            sr2.Close();
-            // Console.ReadLine();
+            Console.ReadLine();
 
         }
     }
