93f14b3c3918e155cb7fde0f0ad5472f49dd8cb2
Michał <michber@onet.pl>
2017-05-22 12:44:36 +0200

    Podstawowa werjsa funckji szyfrującej, na razie tylko dla jednej litery

---
+++ Funkcja Szyfrująca/FunkcjaSzyfrująca/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace FunkcjaSzyfrująca
+{
+    class Program
+    {
+        public int dzialanie(int a, int w, int n)
+        {
+            int pot, wyn, q;
+
+            pot = a; wyn = 1;
+            for (q = w; q > 0; q /= 2)
+            {
+                if (q % 2) wyn = (wyn * pot) % n;
+                pot = (pot * pot) % n;
+            }
+            return wyn;
+        }
+
+        public void kodowanie_RSA(int e, int n, int kod)
+        {
+            int wynik = dzialanie(e, n, kod);
+            Console.WriteLine("\nWynik kodowania = {1}", wynik);
+
+        }
+        public int zmien_na_ascii(char a)
+        {
+            int kod = (int)a;
+            return kod;
+        }
+
+        static void Main(string[] args)
+        {
+            Program n = new Program();
+            int kod = n.zmien_na_ascii('m');
+            n.kodowanie_RSA(20, 40, kod);
+
+        }
+    }
+}
